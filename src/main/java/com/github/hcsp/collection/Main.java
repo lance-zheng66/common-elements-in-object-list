2b2ebb401a36a6aa28ea8215856eff25679b0375
zml59 <zml59@outlook.com>
2020-02-13 23:51:47 +0800

    找公共元素

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,12 +1,22 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，获得a和b集合中的公共元素。
-    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {}
+    public static Set<Person> commonElementsIn(List<Person> a, List<Person> b) {
+        Set<Person> common = new HashSet<>();
+        for (Person person1 : a
+        ) {
+            for (Person person2 : b
+            ) {
+                if (person1.name.equals(person2.name)) {
+                    common.add(person1);
+                }
+            }
+        }
+        return common;
+    }
 
     // Person类，如果两个Person对象的name相等，则认为这两个对象相等。
     public static class Person {
